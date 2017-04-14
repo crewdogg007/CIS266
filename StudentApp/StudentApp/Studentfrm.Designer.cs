357bb633eec2d66be23081833a5c92437abad149
Peter Moore <crewdogg007@gmail.com>
2017-04-14 09:57:45 -0700

    Add files via upload

---
+++ StudentApp/StudentApp/Studentfrm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace StudentApp
+{
+    partial class Studentfrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblAppDate = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblMajor = new System.Windows.Forms.Label();
+            this.cmbMajor = new System.Windows.Forms.ComboBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAppDate = new System.Windows.Forms.TextBox();
+            this.grpResident = new System.Windows.Forms.GroupBox();
+            this.radNo = new System.Windows.Forms.RadioButton();
+            this.radYes = new System.Windows.Forms.RadioButton();
+            this.lblBDay = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtBDay = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lstbxStudent = new System.Windows.Forms.ListBox();
+            this.grpResident.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(36, 26);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(36, 55);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblAppDate
+            // 
+            this.lblAppDate.AutoSize = true;
+            this.lblAppDate.Location = new System.Drawing.Point(304, 26);
+            this.lblAppDate.Name = "lblAppDate";
+            this.lblAppDate.Size = new System.Drawing.Size(85, 13);
+            this.lblAppDate.TabIndex = 2;
+            this.lblAppDate.Text = "Application Date";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(697, 31);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(697, 81);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // lblMajor
+            // 
+            this.lblMajor.AutoSize = true;
+            this.lblMajor.Location = new System.Drawing.Point(36, 78);
+            this.lblMajor.Name = "lblMajor";
+            this.lblMajor.Size = new System.Drawing.Size(33, 13);
+            this.lblMajor.TabIndex = 5;
+            this.lblMajor.Text = "Major";
+            // 
+            // cmbMajor
+            // 
+            this.cmbMajor.FormattingEnabled = true;
+            this.cmbMajor.Items.AddRange(new object[] {
+            "Computer Programming",
+            "Database Managment",
+            "Other",
+            "Undeclared"});
+            this.cmbMajor.Location = new System.Drawing.Point(112, 78);
+            this.cmbMajor.Name = "cmbMajor";
+            this.cmbMajor.Size = new System.Drawing.Size(121, 21);
+            this.cmbMajor.TabIndex = 6;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(112, 23);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 7;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(112, 52);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 8;
+            // 
+            // txtAppDate
+            // 
+            this.txtAppDate.Location = new System.Drawing.Point(395, 19);
+            this.txtAppDate.Name = "txtAppDate";
+            this.txtAppDate.Size = new System.Drawing.Size(100, 20);
+            this.txtAppDate.TabIndex = 9;
+            // 
+            // grpResident
+            // 
+            this.grpResident.Controls.Add(this.radNo);
+            this.grpResident.Controls.Add(this.radYes);
+            this.grpResident.Location = new System.Drawing.Point(15, 243);
+            this.grpResident.Name = "grpResident";
+            this.grpResident.Size = new System.Drawing.Size(200, 100);
+            this.grpResident.TabIndex = 10;
+            this.grpResident.TabStop = false;
+            this.grpResident.Text = "WA Resident";
+            // 
+            // radNo
+            // 
+            this.radNo.AutoSize = true;
+            this.radNo.Location = new System.Drawing.Point(6, 66);
+            this.radNo.Name = "radNo";
+            this.radNo.Size = new System.Drawing.Size(39, 17);
+            this.radNo.TabIndex = 1;
+            this.radNo.TabStop = true;
+            this.radNo.Text = "No";
+            this.radNo.UseVisualStyleBackColor = true;
+            // 
+            // radYes
+            // 
+            this.radYes.AutoSize = true;
+            this.radYes.Location = new System.Drawing.Point(6, 31);
+            this.radYes.Name = "radYes";
+            this.radYes.Size = new System.Drawing.Size(43, 17);
+            this.radYes.TabIndex = 0;
+            this.radYes.TabStop = true;
+            this.radYes.Text = "Yes";
+            this.radYes.UseVisualStyleBackColor = true;
+            // 
+            // lblBDay
+            // 
+            this.lblBDay.AutoSize = true;
+            this.lblBDay.Location = new System.Drawing.Point(316, 57);
+            this.lblBDay.Name = "lblBDay";
+            this.lblBDay.Size = new System.Drawing.Size(54, 13);
+            this.lblBDay.TabIndex = 0;
+            this.lblBDay.Text = "Birth Date";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(320, 138);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(45, 13);
+            this.lblPhone.TabIndex = 1;
+            this.lblPhone.Text = "Phone#";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(18, 138);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtBDay
+            // 
+            this.txtBDay.Location = new System.Drawing.Point(395, 50);
+            this.txtBDay.Name = "txtBDay";
+            this.txtBDay.Size = new System.Drawing.Size(100, 20);
+            this.txtBDay.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(378, 138);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(74, 135);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(188, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // lstbxStudent
+            // 
+            this.lstbxStudent.FormattingEnabled = true;
+            this.lstbxStudent.Location = new System.Drawing.Point(21, 387);
+            this.lstbxStudent.Name = "lstbxStudent";
+            this.lstbxStudent.Size = new System.Drawing.Size(635, 251);
+            this.lstbxStudent.TabIndex = 12;
+            // 
+            // Studentfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 704);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lstbxStudent);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtBDay);
+            this.Controls.Add(this.grpResident);
+            this.Controls.Add(this.txtAppDate);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblBDay);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.cmbMajor);
+            this.Controls.Add(this.lblMajor);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblAppDate);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "Studentfrm";
+            this.Text = "Student Form";
+            this.grpResident.ResumeLayout(false);
+            this.grpResident.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblAppDate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblMajor;
+        private System.Windows.Forms.ComboBox cmbMajor;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtAppDate;
+        private System.Windows.Forms.GroupBox grpResident;
+        private System.Windows.Forms.RadioButton radNo;
+        private System.Windows.Forms.RadioButton radYes;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtBDay;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblBDay;
+        private System.Windows.Forms.ListBox lstbxStudent;
+    }
+}
+
